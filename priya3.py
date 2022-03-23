69942a29f069cb97fd9a80fa8ccaf61322cb5df8
PRIYASHREE ROY <101980665+Priyashree1999@users.noreply.github.com>
2022-03-23 12:55:43 -0500

    Update priya3.py

--- priya3.py
+++ priya3.py
@@ -1,7 +1,7 @@
 l=[(2,5),(1,2),(4,4),(2,3),(2,1)]
 print("sample list1:",l)
-for i in range(len(l)):#5->0-4
-    for j in range((len(l)-i-1)): #5-0-1=4#0-3
+for i in range(len(l))
+    for j in range((len(l)-i-1)): 
         if l[j][1]>l[j+1][1]:#l[0][1]>l[1][1]
             temp=l[j]
             l[j]=l[j+1]
